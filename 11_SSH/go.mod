3141f86bbdffa561ece52d0331cbf71459b59016
Singlewill <156103665@qq.com>
2023-03-14 22:20:07 +0800

    add 11_SSH

--- 11_SSH/go.mod
+++ 11_SSH/go.mod
@@ -1,10 +1,8 @@
 module ssh
 
-go 1.19
+go 1.16
 
 require (
 	github.com/wonderivan/logger v1.0.0
 	golang.org/x/crypto v0.7.0
 )
-
-require golang.org/x/sys v0.6.0 // indirect
--- 11_SSH/main.go
+++ 11_SSH/main.go
@@ -9,50 +9,61 @@ import (
 	"github.com/wonderivan/logger"
 )
 
-func dialSSH(addr string, username string, passwd string, timeout int) {
-	//state := statIDLE
+type SSHClient struct {
+	c *ssh.Client
+}
+
+func NewSSHClient(host string, username string, passwd string, timeout time.Duration) (*SSHClient, error) {
 	config := &ssh.ClientConfig{
-		Timeout:         time.Duration(timeout) * time.Second,
+		Timeout:         timeout * time.Second,
 		User:            username,
 		Auth:            []ssh.AuthMethod{ssh.Password(passwd)},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
 	//创建ssh连接
-	sshClient, err := ssh.Dial("tcp", addr, config)
+	sshClient, err := ssh.Dial("tcp", host, config)
 	if err != nil {
 		logger.Error("ssh Dial Failed : %s\n", err)
-		return
+		return nil, err
 	}
-	defer sshClient.Close()
-	//创建ssh-session
-	session, err := sshClient.NewSession()
+	return &SSHClient{c: sshClient}, nil
+
+}
+
+func (s *SSHClient) Exec(cmd string) ([]byte, error) {
+	session, err := s.c.NewSession()
 	if err != nil {
-		logger.Error("ssh NewSession Failed : %s\n", err)
-		return
+		return nil, err
 	}
 	defer session.Close()
+	return session.CombinedOutput(cmd)
+}
 
-	
-
-	//直接cd到/tmp目录
-	_, err = session.CombinedOutput("cd /tmp")
-	if err != nil {
-		logger.Error("ssh ConbinedOutout Failed : %s\n", err)
-		return
-	}
-	//cat /bin/echo判断架构
-	combo, err := session.CombinedOutput("cat /bin/echo")
-	if err != nil {
-		logger.Error("ssh ConbinedOutout Failed : %s\n", err)
-		return
-	}
-
-	fmt.Println(string(combo))
+func (s *SSHClient) Close() {
+	s.c.Close()
 }
 
 func main() {
 	logger.SetLogger(`{"Console": {"level": "INFO", "color" : true}}`)
-	dialSSH("192.168.1.6:22", "ll", "kalo", 10)
+	ssh, err := NewSSHClient("192.168.1.11:22", "root", "123456", 20)
+	if err != nil {
+		logger.Error("NewSSHClient Faield %s\n", err)
+		return
+	}
+	fmt.Println("create NewSSHClient success")
+	defer ssh.Close()
+	out, err := ssh.Exec("cat /home/123")
+	if err != nil {
+		logger.Warn("Exec Faield %s\n", err)
+	}
+	fmt.Println("11111111111111111111111111111111")
+	fmt.Println(string(out))
+	out, err = ssh.Exec("cd /tmp")
+	if err != nil {
+		logger.Warn("Exec Faield %s\n", err)
+	}
+	fmt.Println("222222222222222222222222222222222")
+	fmt.Println(string(out))
 
 }
